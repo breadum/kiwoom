9d25f4bae70fd40d4659a569a3e1db6903084448
breadum <breadum.kr@gmail.com>
2022-07-05 22:15:57 +0900

    Update to 1.2.3. for handling High Yield Fund.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 # Project Information
-VERSION = "1.2.2"
+VERSION = "1.2.3"
 DISTNAME = "kiwoom"
 DESCRIPTION = "Simple Python Wrapper for Kiwoom Open API+"
 LONG_DESCRIPTION = open("README.md", encoding="utf-8").read()
